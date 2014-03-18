d6752108dd3c4ab46566226ca98eab4a4cbff183
roythuis <roythuis1994@hotmail.com>
2014-03-18 13:25:05 +0100

    includes

--- admin.php
+++ admin.php
@@ -9,31 +9,8 @@
 <body>
 
 <div id="container">
-	<nav>
-		<div id="wrapper">	
-		<ul class="toolbar-list-left">
-			<li class="toolbar-item"><a href="#">Nieuwe klant<i class="fa fa-plus"></i></a></li>
-		</ul>
-
-		<ul class="toolbar-list-right">
-			<li class="toolbar-item alert"><a href="#"><i class="fa fa-bell"></i></a>
-				<ul class="sub-menu-alert">
-					<li>Matt Lauer screeningrapport beschikbaar</li>
-					<li>Erik DeWitt heeft screening afgerond</li>
-					<li>Maatwerkpakket is gewijzigd</li>
-				</ul>
-			</li>
-			<li class="toolbar-item"><a href="#">Logout<i class="fa fa-power-off"></i></a></li>
-			<li class="toolbar-item"><a href="#">Opties<i class="fa fa-cog"></i></a>
-				<ul class="sub-menu-option">
-					<li>Wachtwoord wijzingen</li>
-					<li>E-mail wijzigen</li>
-					<label><li><input type="checkbox">check</li></label>
-				</ul>
-			</li>
-		</ul>
-		</div>
-	</nav>
+	
+	<?php include "toolbar-admin.php"; ?>
 
 	<div id="wrapper">
 
---
+++ toolbar-admin.php
@@ -0,0 +1,18 @@
+<nav>
+		<div id="wrapper">	
+		<ul class="toolbar-list-left">
+			<li class="toolbar-item"><a href="#">Nieuw bedrijf<i class="fa fa-plus"></i></a></li>
+		</ul>
+
+		<ul class="toolbar-list-right">
+			<li class="toolbar-item"><a href="#">Logout<i class="fa fa-power-off"></i></a></li>
+			<li class="toolbar-item"><a href="#">Opties<i class="fa fa-cog"></i></a>
+				<ul class="sub-menu-option">
+					<li>Wachtwoord wijzingen</li>
+					<li>E-mail wijzigen</li>
+					<label><li><input type="checkbox">check</li></label>
+				</ul>
+			</li>
+		</ul>
+		</div>
+	</nav>
