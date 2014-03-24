3b8854b8fd9464587e9cacc486136452078526ed
roythuis <roythuis1994@hotmail.com>
2014-03-24 12:16:32 +0100

    update new-pw.php

--- new-pw.php
+++ new-pw.php
@@ -3,6 +3,7 @@
 <head>
 	<title>Nieuw Wachtwoord</title>
 	<link rel="stylesheet" type="text/css" href="styles/main.css">
+	<link rel="stylesheet" href="font-awesome-4.0.3/css/font-awesome.min.css">
 </head>
 <body>
 
@@ -18,11 +19,17 @@
 		<form id="new-pw-form" name="new-pw-form" method="post" action="<?php $_SERVER['PHP_SELF']; ?>">
 			<div class="content-block new-pw-block">
 				<p>Omdat dit de eerste keer is dat u inlogt, is er een nieuw wachtwoord nodig.</p>
-				<p class="new-pw-block-grey">Minimaal 6 karakters en moet een cijfer en een hoofdletter bevatten.</p>
-				<label for="new-password">Nieuw wachtwoord</label>
-				<label for="new-password-repeat">Herhaal wachtwoord</label><br>
-				<input type="password" name="new-password" id="new-password">
-				<input type="password" name="new-password-repeat" id="new-password-repeat">
+				<p class="comment">Wachtwoord moet minimaal 6 karakters, een cijfer en een hoofdletter bevatten.</p>
+				<table id="new-pw-table">
+					<tr>
+						<td><label for="new-password">Nieuw wachtwoord</label></td>
+						<td><label for="new-password-repeat">Herhaal wachtwoord</label></td>
+					</tr>
+					<tr>
+						<td><input type="password" name="new-password" id="new-password"></td>
+						<td><input type="password" name="new-password-repeat" id="new-password-repeat"></td>
+					</tr>
+				</table>
 			</div>
 			<input type="submit" name="submit" value="Opslaan">
 		</form>
@@ -32,4 +39,4 @@
 </div>
 
 </body>
-</html>
+</html>			
