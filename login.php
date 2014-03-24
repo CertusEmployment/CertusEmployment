0f8b1747e856cb90b13c175302ed9fa9721c26f6
Cairan Steverink <cairan_432@hotmail.com>
2014-03-24 12:03:39 +0100

    login / pwreset
    login / css / pw-reset

---
+++ login.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+	<title>Login</title>
+	<link rel="stylesheet" type="text/css" href="styles/main.css" />
+</head>
+<body>
+
+
+<div id="container">
+	<div id="login-block">
+		<img src="styles/images/certus_logo.png" />
+
+		<form name="login">
+			<label id="username">Gebruikersnaam:</label><br />
+			<input class="margin" type="text" name="username" id="username" /><br />
+
+			<label id="password">Wachtwoord:</label><br />
+			<input class="no-padding" type="password" name="password" id="password" /><br />
+
+			<a class="reset-password" href="#">Wachtwoord vergeten</a><br />
+			<input type="submit" name="Submit" value="Inloggen" />
+		</form>
+	</div>
+</div>
+
+</body>
+</html>
