e9d6e74a799291540e6ca80becfe696c24acbee1
roythuis <roythuis1994@hotmail.com>
2014-03-26 11:27:53 +0100

    edits

---
+++ password-forget.php
@@ -0,0 +1,45 @@
+<html>
+<head>
+	<title>Wachtwoord vergeten</title>
+
+	<link rel="stylesheet" type="text/css" href="styles/main.css" media="screen" />
+	<link rel="stylesheet" href="font-awesome-4.0.3/css/font-awesome.min.css">
+
+</head>
+<body>
+
+<div id="container">
+
+	<div id="wrapper">
+
+		<div id="logo">
+			<img src="styles/images/certus_logo.png" />
+		</div>
+
+		<div class="content-block">
+			<div class="settings-block">
+				<h3 >Wachtwoord herstellen</h3>
+				<p>
+					Geef het e-mailadres op waarmee u geregistreerd staat in het systeem.
+					Wanneer u later uw e-mailadres heeft gewijzigd bij uw gegevens, geeft 
+					u dan het gewijzigde e-mailadres op. Er kan dan voor u een link worden 
+					gegenereerd, waar u uw wachtwoord kunt aanpassen.
+				</p>
+				<form id="settings-form" name="password-forget">
+					<label for="password-forget">E-mail adres: </label>
+					<input id="password-forget" type="text" name="email">
+					<input type="submit" value="Verzenden" name="submit">
+				</form>
+
+				<p class="comment">
+					Wanneer u niet meer weet met welk e-mailadres u bent geregistreerd kunt
+					u contact op nemen met info@certus-employment.nl om deze op te vragen.
+				</p>
+				<a href="#">Terug naar de inlog pagina</a>
+				
+			</div>
+		</div>
+	</div>
+</div>
+</body>
+</html>
