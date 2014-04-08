46929ad7ac03b91407dbe0de9d2f791a69f2c22b
roythuis <roythuis1994@hotmail.com>
2014-04-08 10:31:13 +0200

    changePassword

--- admin/admin-editemail.php
+++ admin/admin-editemail.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Wachtwoord wijzigen</title>
+	<title>Email wijzigen</title>
 	<link rel="stylesheet" type="text/css" href="../styles/main.css">
 	<link rel="stylesheet" href="../font-awesome-4.0.3/css/font-awesome.min.css">
 </head>
@@ -19,20 +19,15 @@
 		
 		<form id="settings-form" name="settings-form" method="post" action="<?php $_SERVER['PHP_SELF']; ?>">
 			<div class="content-block settings-block">
+				<p>Huidig e-mail adres: mail@mail.com</p>
 				<table id="settings-table">
 					<tr>
-						<td><label for="old-pw">Huidig wachtwoord</label></td>
+						<td><label for="new-password">Nieuw e-mail adres</label></td>
+						<td><label for="new-password-repeat">Herhaal e-mail adres</label></td>
 					</tr>
 					<tr>
-						<td><input type="password" name="old-pw" id="old-pw"></td>
-					</tr>
-					<tr>
-						<td><label for="new-password">Nieuw wachtwoord</label></td>
-						<td><label for="new-password-repeat">Herhaal wachtwoord</label></td>
-					</tr>
-					<tr>
-						<td><input type="password" name="new-password" id="new-password"></td>
-						<td><input type="password" name="new-password-repeat" id="new-password-repeat"></td>
+						<td><input type="text" name="new-password" id="new-password"></td>
+						<td><input type="text" name="new-password-repeat" id="new-password-repeat"></td>
 					</tr>
 				</table>
 			</div>
