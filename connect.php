01716439cc4fc4f9a08ac4a4e4c75a3f0684d090
roythuis <roythuis1994@hotmail.com>
2014-04-14 14:26:08 +0200

    connect.php

---
+++ connect.php
@@ -0,0 +1,11 @@
+<?php
+
+$host = 'localhost';
+$dbnaam = 'certustest';
+$user = 'root';
+$passw = '';
+
+$db = mysql_connect($host,$user,$passw) or die(mysql_error());
+mysql_select_db($dbnaam, $db) or die(mysql_error());
+
+?>
