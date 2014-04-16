5521db16310507d3cf1f3e489be56c547a71154b
Cairan Steverink <cairan_432@hotmail.com>
2014-04-16 13:20:30 +0200

    fast
    fast

--- connect.php
+++ connect.php
@@ -3,7 +3,7 @@
 $host = 'localhost';
 $dbnaam = 'certustest';
 $user = 'root';
-$passw = 'Steverink23';
+$passw = '';
 
 $db = mysql_connect($host,$user,$passw) or die(mysql_error());
 mysql_select_db($dbnaam, $db) or die(mysql_error());
