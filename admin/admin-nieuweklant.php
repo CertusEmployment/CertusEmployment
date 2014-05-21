31ceb1d1a9c405484ddd269883ce1d3e6f05be6a
Cairan Steverink <cairan_432@hotmail.com>
2014-05-21 13:23:58 +0200

    integriteit
    integriteit gebeuren

--- admin/admin-nieuweklant.php
+++ admin/admin-nieuweklant.php
@@ -152,11 +152,7 @@ if(!$posting) {
 						<td><label for="plaats">Plaats</label></td>
 					</tr>
 					<tr>
-<<<<<<< HEAD
-						<td><input type="text" value="<?php echo $_POST['postcode']; ?>" id="postcode" name="postcode" required></td>
-=======
 						<td><input type="text" value="<?php echo $_POST['postcode']; ?>" id="postcode" name="postcode" placeholder="1234AA" required></td>
->>>>>>> FETCH_HEAD
 						<td><input type="text" value="<?php echo $_POST['plaats']; ?>" id="plaats" name="plaats" required></td>
 					</tr>
 					<tr>
